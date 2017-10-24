6f1dc8b915afde2f5c1fdb45056ba519a2ba8897
Ryan Thies <rnthies@gmail.com>
2017-10-23 20:46:41 -0500

    Lesson 8 Code Added During Class

---
+++ LESSON8/lesson8_starter_code/[0] boxes/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	
+	/* Your JS here */
+
+	// After creating your functions below, if they need to run on page load put them here
+	yourFunction();
+
+});
+
+// If you need to write functions create them down here
+function yourFunction() {
+
+}
